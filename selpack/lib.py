cdbd8198cdc8ff7519d44df4cad552151ec70e8f
Selim Kebaier <selimkebaier@gmail.com>
2021-08-17 01:09:41 +0200

    lib modification

--- selpack/lib.py
+++ selpack/lib.py
@@ -1,6 +1,8 @@
 def try_me():
     name = input ("what's your name?")
-    if name.lower() == 'jean remi' or name.lower() == 'morgan':
-        print ('casse toi tocard')
+    if 'jean ' in name.lower() or 'jean-' in name.lower():
+        print ('Salut fil de fer')
+    elif name.lower() == 'morgan':
+        print("File t'entrainer! Louis t'attend à la salle")
     else:
         print (f'Bonjour {name}, bienvenu!!!')
