7c9459a8e6d5eb50496e202775224e16c9a0d2ff
Selim Kebaier <selimkebaier@gmail.com>
2021-08-16 16:50:51 +0200

    Initial Push

--- selpack/lib.py
+++ selpack/lib.py
@@ -1,6 +1,6 @@
 def try_me():
     name = input ("what's your name?")
-    if name.lower() == 'jean rémi' or name.lower() == 'morgan':
+    if name.lower() == 'jean remi' or name.lower() == 'morgan':
         print ('casse toi tocard')
     else:
         print (f'Bonjour {name}, bienvenu!!!')
